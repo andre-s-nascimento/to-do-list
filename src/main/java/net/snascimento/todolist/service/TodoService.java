c646e35865873f6843f6f2eaf44847f69c8512ac
Andre S Nascimento <andresoaresnascimento@gmail.com>
2023-09-01 15:17:43 -0300

    Entidade, Repositorio, Controller e Services - ok

--- src/main/java/net/snascimento/todolist/service/TodoService.java
+++ src/main/java/net/snascimento/todolist/service/TodoService.java
@@ -1,9 +1,40 @@
 package net.snascimento.todolist.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import net.snascimento.todolist.entity.Todo;
+
+import net.snascimento.todolist.repository.TodoRepository;
 
 @Service
 public class TodoService {
 
-    
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list() {
+        Sort sort = Sort.by("prioridade").descending().and(Sort.by("nome").ascending());
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> delete(Long id) {
+        todoRepository.deleteById(id);
+        return list();
+    }
+
 }
