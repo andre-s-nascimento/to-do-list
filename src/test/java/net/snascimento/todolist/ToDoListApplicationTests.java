25537bea880dbbbea96bedcc590441436256a0e6
Andre S Nascimento <andresoaresnascimento@gmail.com>
2023-09-03 19:49:17 -0300

    Adjust in imports.

--- src/test/java/net/snascimento/todolist/ToDoListApplicationTests.java
+++ src/test/java/net/snascimento/todolist/ToDoListApplicationTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
